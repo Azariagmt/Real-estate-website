8b62bcb1409d1d00c19b5c26b34afb49a7d34b5d
Azaria Gebremichael <azariagebremichael10@gmail.com>
2020-03-21 13:05:13 +0300

    instant search mess created

---
+++ .history/public/js/algolia-instantsearch_20200321023241.js
@@ -0,0 +1,75 @@
+(function() {
+    const search = instantsearch({
+        appId: 'NI2IZ7VM3G',
+        apiKey: '19783030ede986f9e969322c65ccc03c',
+        indexName: 'propertie',
+        urlSync: true
+    });
+
+    search.addWidget(
+        instantsearch.widgets.hits({
+            container: '#hits',
+            templates: {
+                empty: 'No results',
+                item: function(item) {
+                    return `
+                    <a href="${window.location.origin}/shop/${item.slug}">
+                    <div class="instantsearch-result">
+                        <div>
+                            <img src="${window.location.origin}/storage/${item.image}" alt="img" class="algolia-thumb-result" width="200px">
+                        </div>
+                        <div>
+                            <div class="result-title">
+                                ${item._highlightResult.name.value}
+                            </div>
+                            <div class="result-details">
+                                ${item._highlightResult.details.value}
+                            </div>
+                            <div class="result-price">
+                                ${(item.price).toFixed(2)} ETB
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                        <hr>
+                    `;
+                }
+            }
+        })
+    );
+
+    // initialize SearchBox
+    search.addWidget(
+        instantsearch.widgets.searchBox({
+            container: '#search-box',
+            placeholder: 'Search for products'
+        })
+    );
+
+    // initialize pagination
+    search.addWidget(
+        instantsearch.widgets.pagination({
+            container: '#pagination',
+            maxPages: 20,
+            // default is to scroll to 'body', here we disable this behavior
+            scrollTo: false
+        })
+    );
+
+    search.addWidget(
+        instantsearch.widgets.stats({
+            container: '#stats-container'
+        })
+    );
+
+    // initialize RefinementList
+    search.addWidget(
+        instantsearch.widgets.refinementList({
+            container: '#refinement-list',
+            attributeName: 'categories',
+            sortBy: ['name:asc']
+        })
+    );
+
+    search.start();
+})();
---
+++ .history/routes/web_20200321022300.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'HomeController@getIndex')->name('landing-page');
+
+Route::get('/contact', 'ContactController@getContactForm')->name('contact');
+
+Route::get('/aboutUs', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/properties', 'PropertyController@getPropertiesPage')->name('property');
+
+Route::get('/properties/{property}', 'PropertyController@getSelectedPropertiesPage')->name('property-selected');
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
+Route::get('/search-algolia', 'PropertyController@searchAlgolia')->name();
+Route::get('pagenotfound', 'HomeController@pageNotFound')->name('not-found');
+
+Route::get('/selling','PropertyController@getSellingPage')->name('selling');
+
+Route::group(['prefix' => 'laravel-filemanager', 
+'middleware' => ['web', 'auth']], function () {
+    \UniSharp\LaravelFilemanager\Lfm::routes();});
+
